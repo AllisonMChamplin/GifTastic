a0b071f32ca3dc0d96b073ab48d8a1d63885646f
Allison Champlin <47014575+AllisonMChamplin@users.noreply.github.com>
2019-05-18 17:14:07 -0500

    Restructured the javascript

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,95 +1,104 @@
-
-// Array of topics
 $(document).ready(function () {
 
-    var topics = ["Rainbows", "Fairies", "Unicorns", "Sparkles", "Princesses", "Pink kittens"];
+    var topicsArray = ["Rainbows", "Fairies", "Unicorns", "Sparkles", "Princesses", "Pink kittens", "Ponies", "Twilight sparkle", "Pinkie Pie", "Rainbow Dash", "Fluttershy"];
     var colors = ["c8bddd", "a68fc3", "a94698", "7754a4", "652d92", "8b2475", "c41a7b", "ef1468", "e46075", "ed6967", "c82262"];
 
+    // This function creates a button for each item in topicsArray and appends them to #tags
     var topicList = function () {
-        console.log("hi topicList");
+        console.log("---- hi topicList ----");
         $('#tags').empty();
-        console.log("Clearing tags div");
-        for (var i = 0; i < topics.length; i++) {
+        // Create a button for each topic in topicsArray
+        for (var i = 0; i < topicsArray.length; i++) {
+            // Variable to hold the new button
             var tagDiv = $("<button>");
-            var randomColor = colors[Math.floor(Math.random() * colors.length)];
-            tagDiv.html(topics[i]);
+            // Assign each button a random color (for fun)
+            tagDiv.css("background-color", "#" + randomColor());
+            // Add the current topic from topicsArray into the button HTML
+            tagDiv.html(topicsArray[i]);
+            // Add class and identifier attributes to the button
             tagDiv.attr("class", "tag");
-            tagDiv.attr("data-topic", topics[i]);
-            tagDiv.css("background-color", "#" + randomColor);
+            // Store the cooresponding topic in the data-topic attribute
+            tagDiv.attr("data-topic", topicsArray[i]);
+            // Append the button to #tags
             $('#tags').append(tagDiv);
         }
+    };
 
-        $(".tag").on("click", function () {
-            // Grabbing and storing the data-topic property value from the button
-            var topic = $(this).attr("data-topic");
-            console.log("topic ", topic);
+    // Rando colors (called by topicList)
+    var randomColor = function () {
+        console.log("---- hi randomColor ----");
+        var c = colors[Math.floor(Math.random() * colors.length)];
+        return c;
+    };
 
-            // Constructing a queryURL using the animal name
-            var APIKEY = "DPBvGpuy5v0lsWlSAd51dsjMvJ6rjWcP";
-            var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-                topic + "&api_key=" + APIKEY + "&limit=10";
-            console.log("queryURL: ", queryURL);
-            // Performing an AJAX request with the queryURL
-            $.ajax({
-                url: queryURL,
-                method: "GET"
-            })
-                // After data comes back from the request
-                .then(function (response) {
-                    console.log(queryURL);
-                    console.log(response);
+    // Ajax call to GiphyAPI using parameter from #tags listener
+    var gifRequest = function (topic) {
+        console.log("---- hi gifRequest ----");
+        // Constructing a queryURL using the topic name
+        var APIKEY = "DPBvGpuy5v0lsWlSAd51dsjMvJ6rjWcP";
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+            topic + "&api_key=" + APIKEY + "&limit=10";
+        // Performing an AJAX request with the queryURL
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+            // After data comes back from the request
+            .then(function (response) {
+                console.log(queryURL);
+                console.log(response);
 
-                    // storing the data from the AJAX request in the results variable
-                    var results = response.data;
+                // storing the data from the AJAX request in the results variable
+                var results = response.data;
+                displayGifs(results);
+            });
+    };
 
-                    // Looping through each result item
-                    for (var i = 0; i < results.length; i++) {
-
-                        // Creating and storing a div tag
-                        var topicDiv = $("<div>");
-
-                        // Creating a paragraph tag with the result item's rating
-                        var p = $("<p>").text("Rating: " + results[i].rating);
-
-                        // Creating and storing an image tag
-                        var topicImage = $("<img>");
-                        // Setting the src attribute of the image to the fixed height still property
-                        topicImage.attr("src", results[i].images.fixed_height_still.url);
-
-                        topicImage.attr("data-state", "still");
-                        topicImage.attr("data-still", results[i].images.fixed_height_still.url);
-                        topicImage.attr("data-animate", results[i].images.fixed_height.url);
-                        // Setting the 
-                        // Adding a class to the image
-                        topicImage.attr("class", "clicky");
-                        // Appending the paragraph and image tag to the topicDiv
-                        topicDiv.attr("class", "topic-image");
-                        topicDiv.append(p);
-                        topicDiv.append(topicImage);
-
-                        // Prependng the topicDiv to the HTML page in the "#gifs-appear-here" div
-                        $("#gifs-appear-here").prepend(topicDiv);
-                    }
-
-                });
-        });
-
-
-    }
-
-    topicList();
-
-
+    // Function to add ajax array to the page
+    var displayGifs = function (x) {
+        console.log("---- hi displayGifs ----");
+        var resultsList = x;
+        // Looping through each result item
+        for (var i = 0; i < resultsList.length; i++) {
+            // Creating and storing a div tag
+            var topicDiv = $("<div>");
+            // Creating a paragraph tag with the result item's rating
+            var p = $("<p>").text("Rating: " + resultsList[i].rating.toUpperCase());
+            p.attr("class", "meta");
+            // Creating and storing an image tag
+            var topicImage = $("<img>");
+            // Setting the src attribute of the image to the fixed height still property
+            topicImage.attr("src", resultsList[i].images.fixed_height_still.url);
+            topicImage.attr("data-state", "still");
+            topicImage.attr("data-still", resultsList[i].images.fixed_height_still.url);
+            topicImage.attr("data-animate", resultsList[i].images.fixed_height.url);
+            // Setting the 
+            // Adding a class to the image
+            topicImage.attr("class", "clicky");
+            // Appending the paragraph and image tag to the topicDiv
+            topicDiv.attr("class", "topic-image");
+            topicDiv.append(topicImage);
+            topicDiv.append(p);
+            // Prependng the topicDiv to the HTML page in the "#gifs-appear-here" div
+            $("#gifs-appear-here").prepend(topicDiv);
+        }
+    };
+    
+    // Click listener for buttons inside #tags div
+    $("#tags").on("click", "button", function () {
+        console.log("---- hi #tags listener ----");
+        // Storing the data-topic property value from the button
+        var topic = $(this).attr("data-topic");
+        console.log("topic ", topic);
+        gifRequest(topic);
+    });
 
     // Click handler to toggle between still image and animated GIFs
     $('body').on('click', 'img', function () {
-        console.log('it works');
-
-        // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+        console.log('---- hi body listener ----');
+        // Store the data state attribute
         var state = $(this).attr("data-state");
-        // If the clicked image's state is still, update its src attribute to what its data-animate value is.
-        // Then, set the image's data-state to animate
-        // Else set src to the data-still value
+        // Toggle the data state on click
         if (state === "still") {
             $(this).attr("src", $(this).attr("data-animate"));
             $(this).attr("data-state", "animate");
@@ -97,53 +106,29 @@ $(document).ready(function () {
             $(this).attr("src", $(this).attr("data-still"));
             $(this).attr("data-state", "still");
         }
-    })
-
-    var updatePage = function () {
-        console.log("hi updatePage");
-
-    }
-
+    });
 
     // Click Handler for the Search Button
     $("#run-search").on("click", function (event) {
+        console.log("---- hi run search listener ----");
         // Prevents the page from reloading on form submit.
         event.preventDefault();
-
-        console.log("Hi run search click handler");
-
         // Grab text the user typed into the search input
         var input = $("#search-term").val().trim();
-        console.log("input *****", input);
-
+        // If the input is not empty, add the topic to the array
         if (input) {
-        topics.push(input);
-        console.log("topics array: ", topics);
-        topicList();
+            topicsArray.push(input);
+            topicList();
         };
-
-
-        //     // Constructing a queryURL using the animal name
-        //     var APIKEY = "DPBvGpuy5v0lsWlSAd51dsjMvJ6rjWcP";
-        //     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        //         input + "&api_key=" + APIKEY + "&limit=10";
-
-
-        //     // Make the AJAX request to the API - GETs the JSON data at the queryURL.
-        //     // The data then gets passed as an argument to the updatePage function
-        //     $.ajax({
-        //         url: queryURL,
-        //         method: "GET"
-        //     }).then(function (response) {
-
-        //         console.log(queryURL);
-        //         console.log(response);
-
-        //         // storing the data from the AJAX request in the results variable
-        //         var results = response.data;
-
     });
+
+    // Initialize function
+    var init = function () {
+        console.log("---- hi init ----");
+        topicList();
+    };
+
+    // Start the 'app'
+    init();
+
 });
-
-
-
